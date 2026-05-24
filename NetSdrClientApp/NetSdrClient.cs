78b60e6f6f03d15eed9e80c2b93a9ab6d474fcfa
Yaroslav Fedyna <fedynaslava@gmail.com>
2026-05-24 12:13:20 +0300

    make private fields readonly

--- NetSdrClientApp/NetSdrClient.cs
+++ NetSdrClientApp/NetSdrClient.cs
@@ -6,8 +6,8 @@ namespace NetSdrClientApp
 {
     public class NetSdrClient
     {
-        private ITcpClient _tcpClient;
-        private IUdpClient _udpClient;
+        private readonly ITcpClient _tcpClient;
+        private readonly IUdpClient _udpClient;
 
         public bool IQStarted { get; set; }
 
